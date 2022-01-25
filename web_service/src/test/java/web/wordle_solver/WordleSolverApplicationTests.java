8989e4b47680167732a231cb0fafe5ffc3e40173
EC2 Default User <kindzerske@gmail.com>
2022-01-25 20:37:39 +0000

    /web_service now plugged into CI for unittests, with starter tests

--- web_service/src/test/java/web/wordle_solver/WordleSolverApplicationTests.java
+++ web_service/src/test/java/web/wordle_solver/WordleSolverApplicationTests.java
@@ -1,13 +1,37 @@
 package web.wordle_solver;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @SpringBootTest
-class WordleSolverApplicationTests {
+@AutoConfigureMockMvc
+public class WordleSolverApplicationTests {
+
+	@Autowired
+	private MockMvc mvc;
 
 	@Test
-	void contextLoads() {
+	public void webServiceIndex() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("Index of WorldSolver!")));
+	}
+
+	@Test
+	public void demoTrieEndpoint() throws Exception {
+		mvc.perform(MockMvcRequestBuilders.get("/demoTrie").accept(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(content().string(equalTo("[wince]")));
 	}
 
 }
